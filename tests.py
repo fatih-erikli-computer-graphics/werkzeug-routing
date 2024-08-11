aa7261d06d23912e2a2e9e7be5169188324c33fa
Fatih Erikli <fatih.erikli.work@gmail.com>
2024-08-11 14:53:05 +0300

    Add domain test

--- tests.py
+++ tests.py
@@ -6,6 +6,8 @@ add_rule("/register/<referrer>", lambda referrer: "register referrer" + referrer
 add_rule("/<icon_set>/<name>.png", lambda icon_set, name: "icon:" + icon_set + name)
 add_rule("/<user>/pic/<picid>", lambda user, picid: "userpic:" + user + picid)
 add_rule("/<user>", lambda user: "user:" + user)
+add_rule("<subdomain>.<domain>.com", lambda subdomain, domain: subdomain+domain)
+add_rule("<subdomain>.<domain>.com/<path>", lambda subdomain, domain, path: subdomain+domain+path)
 
 def route(path):
   match = resolve(path)
@@ -17,3 +19,5 @@ assert route("/register/kartonpr") == "register referrerkartonpr"
 assert route("/solitaire/diamond.png") == "icon:solitairediamond"
 assert route("/ramm") == "user:ramm"
 assert route("/ramm/pic/2") == "userpic:ramm2"
+assert route("abc.def.com") == "abcdef"
+assert route("abc.def.com/login") == "abcdeflogin"
